72d20ff2923e2e427fa03b7ea1a53c5ef7556f20
Никита <nikita.dyumin@gmail.com>
2015-10-18 09:05:14 +0500

    added "\name NAME" command

--- src/server.erl
+++ src/server.erl
@@ -16,7 +16,7 @@
 
 listen() ->
   {ok, LSock} = gen_tcp:listen(6667, ?TCP_OPTIONS),
-  Server = spawn(fun() -> server_loop([]) end),
+  Server = spawn(fun() -> server_loop(maps:new()) end),
   tcp_connection_loop(Server, LSock).
 
 tcp_connection_loop(Server, LSock) ->
@@ -27,19 +27,21 @@ tcp_connection_loop(Server, LSock) ->
 
 server_loop(Connections) ->
   receive
-    {connected, Socket} -> server_loop([Socket | Connections]);
+    {connected, Socket} -> server_loop(maps:put(Socket, "", Connections));
     {disconnected, Socket} -> server_loop(list_without(Socket, Connections));
+    {name, Socket, Name} -> server_loop(maps:update(Socket, Name ++ ": ", Connections));
     {msg, Receiver, Data} ->
-      sendout_message(list_without(Receiver, Connections), Data),
+      sendout_message(list_without(Receiver, Connections), maps:get(Receiver, Connections) ++ Data),
       server_loop(Connections);
     {close} -> ok
   end.
 
 list_without(Item, List) ->
-  lists:filter(fun(C) -> C /= Item end, List).
+  maps:remove(Item, List).
 
 sendout_message(Connections, Msg) ->
-  lists:map(fun(C) -> gen_tcp:send(C, Msg) end, Connections).
+  List = maps:keys(Connections),
+  lists:map(fun(C) -> gen_tcp:send(C, Msg) end, List).
 
 client_loop(Server, Socket, Acc) ->
   case gen_tcp:recv(Socket, 0) of
@@ -59,7 +61,9 @@ parse_message(Server, Client, Msg) ->
   case strip_eol(Msg) of
     "\\help" -> gen_tcp:send(Client, "available commands:\r\n help\r\n greetme\r\n name YOUR_NAME\r\n");
     "\\greetme" -> gen_tcp:send(Client, "hi\r\n");
-    "\\name " ++ Name -> gen_tcp:send(Client, "your name is: " ++ Name ++ "\r\n");
+    "\\name " ++ Name ->
+      Server ! {name, Client, Name},
+      gen_tcp:send(Client, "your name is: " ++ Name ++ "\r\n");
     "\\" ++ Cmd -> gen_tcp:send(Client, "invalid command: " ++ Cmd ++ "\r\n");
     _ -> Server ! {msg, Client, Msg}
   end.
